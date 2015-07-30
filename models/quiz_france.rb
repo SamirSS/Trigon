faaefc1e9cadb15b9f195bccdb5e9ca532503408
itskt <katieko98@gmail.com>
2015-07-30 19:15:44 +0000

    KT's work

---
+++ models/quiz_france.rb
@@ -0,0 +1,29 @@
+class Travel
+  attr_reader = :determine_location
+  
+  def initialize(params)
+    @results = params
+    determine_location
+    @paris = {:city => "Paris"}
+    @perouges = {:city => "Perouges"}
+  end
+  
+  def determine_location 
+    if @results[:environment] == "busy" && @results[:preference] == "warm" && @results[:water] == "nemo"
+      return {:city => "Marseille"}
+    elsif @results[:environment] == "busy" && @results[:preference] == "warm" && @results[:water] == "cat"
+      @paris
+    elsif @results[:environment] == "busy" && @results[:preference] == "cool" && @results[:water] == "nemo"
+      @paris
+    elsif @results[:environment] == "busy" && @results[:preference] == "cool" && @results[:water] == "cat"
+      return {:city => "Lyon"}
+    elsif @results[:environment] == "quiet" && @results[:preference] == "warm" && @results[:water] == "nemo"
+      @perouges
+    elsif @results[:environment] == "quiet" && @results[:preference] == "warm" && @results[:water] == "cat"
+      @perouges
+    elsif @results[:environment] == "quiet" && @results[:preference] == "cool" && @results[:water] == "nemo"
+      return {:city => "Annecy"}
+    elsif @results[:environment] == "quiet" && @results[:preference] == "warm" && @results[:water] == "nemo"
+      return {:city => "Saint Veran"}
+   end
+end
---
+++ models/quiz_us.rb
@@ -0,0 +1,11 @@
+class Travel
+  attr_reader = :determine_location
+  
+  def initialize
+    @results = results
+    determine_location
+  end
+  
+  def determine_location 
+    if @results[:first] == 
+end
